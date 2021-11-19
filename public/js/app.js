7630cf1365dc913fcb2b655f5bdb5d1f7754103c
imsandeepan <shondha.tara94@gmail.com>
2021-11-20 01:24:32 +0530

    Remove unnecessary console.log()

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,8 @@
-
-console.log('Client side javaScript!')
-
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msgOne = document.querySelector('#msg-1')
 const msgTwo = document.querySelector('#msg-2')
-console.log(search)
+
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -17,13 +11,13 @@ weatherForm.addEventListener('submit', (e) => {
     response.json().then((data) => {
         if(data.error) {
             msgOne.textContent = data.error
-            console.log(data.error)
+            
         } else {
-            console.log('wind speed', data.windSpeed, 'temperature', data.temperature)
+            // console.log('wind speed', data.windSpeed, 'temperature', data.temperature)
             msgOne.textContent = data.temperature
             msgTwo.textContent = data.windSpeed
         }
     })
 })
-    console.log(location)
+    
 })
